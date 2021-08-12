a6f13e141dc709697242f43bf0d5a0fba0f64575
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2021-08-12 11:10:22 +0900

    [doce] RemoveDuplicateletters

---
+++ src/string/RemoveDuplicateLetters.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class RemoveDuplicateLetters {
+
+	public String solution(String str) {
+		String answer = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			// System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
+			if (str.indexOf(str.charAt(i)) == i)
+				answer += str.charAt(i);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		RemoveDuplicateLetters T = new RemoveDuplicateLetters();
+		Scanner kb = new Scanner(System.in);
+
+		String str = kb.next();
+
+		System.out.print(T.solution(str));
+	}
+}
