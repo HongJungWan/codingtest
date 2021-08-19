65eee8e79fc4b965a034658eaddc1bc820713ce3
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2021-08-20 07:55:04 +0900

    [doce] Student

--- src/array/Student.java
+++ src/array/Student.java
@@ -1,10 +1,34 @@
 package array;
 
+import java.util.Scanner;
+
 public class Student {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int solution(int n, int[] arr) {
+		int answer = 1;
+		int max = arr[0];
 
+		for (int i = 1; i < n; i++) {
+			if (arr[i] > max) {
+				answer++;
+				max = arr[i];
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(student.solution(n, arr));
 	}
 
 }
