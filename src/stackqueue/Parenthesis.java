2789ddde14a9f8fafe059d46bb5bd996e35d8c50
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2021-09-19 18:15:40 +0900

    [doce] src/stackqueue

---
+++ src/stackqueue/Parenthesis.java
@@ -0,0 +1,33 @@
+package stackqueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Parenthesis {
+
+	public String solution(String str) {
+		String answer = "YES";
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for (char x : str.toCharArray()) {
+			if (x == '(') stack.push(x);
+			else {
+				if (stack.isEmpty()) return "NO";
+				stack.pop();
+			}
+		}
+		if (!stack.isEmpty()) return "NO";
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Parenthesis T = new Parenthesis();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		System.out.println(T.solution(str));
+	}
+
+}
