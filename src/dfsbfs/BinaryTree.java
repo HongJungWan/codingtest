1e0960741cc1ad07f8f2a5e151a3e8a93095ed45
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2021-10-04 23:23:01 +0900

    [doce] BinaryTree

---
+++ src/dfsbfs/BinaryTree.java
@@ -0,0 +1,41 @@
+package dfsbfs;
+
+class Node {
+	int data;
+	Node lt, rt;
+
+	public Node(int val) {
+		data = val;
+		lt = rt = null;
+	}
+}
+
+public class BinaryTree {
+	Node root;
+
+	public void DFS(Node root) {
+		if (root == null) return;
+		else {
+			DFS(root.lt);
+			System.out.print(root.data + " ");
+			DFS(root.rt);
+		}
+	}
+
+	public static void main(String args[]) {
+		BinaryTree tree = new BinaryTree();
+		
+		tree.root = new Node(1);
+		
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		
+		tree.root.rt.lt = new Node(6);
+		tree.root.rt.rt = new Node(7);
+		
+		tree.DFS(tree.root);
+	}
+}
